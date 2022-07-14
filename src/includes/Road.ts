51e7557783c38c15ceefc44a73422a2fe2588792
ojpro <contact@ojpro.me>
2022-07-14 19:55:23 +0100

    added road class

---
+++ src/includes/Road.ts
@@ -0,0 +1,53 @@
+import {lerp} from "./Utils";
+
+export default class Road {
+    private x: any;
+    private laneCount: number;
+    private width: any;
+    private left: number;
+    private right: any;
+    private top: number;
+    private bottom: number;
+
+    constructor(x, width, laneCount = 3) {
+        this.x = x
+        this.width = width
+        this.laneCount = laneCount
+
+        this.left = x - width / 2
+        this.right = x + width / 2
+
+        const infinity = 10000000
+        this.top = -infinity
+        this.bottom = infinity
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        ctx.lineWidth = 5
+        ctx.strokeStyle = 'white'
+
+        for (let i = 0; i <= this.laneCount; i++) {
+            const x = lerp(
+                this.left,
+                this.right,
+                i / this.laneCount
+            )
+
+            if (i > 0 && i < this.laneCount) {
+                ctx.setLineDash([20, 20])
+            } else {
+                ctx.setLineDash([])
+            }
+
+            ctx.beginPath()
+            ctx.moveTo(x, this.top)
+            ctx.lineTo(x, this.bottom)
+            ctx.stroke()
+        }
+    }
+
+    getLaneCenter(laneIndex) {
+        const laneWidth = this.width / this.laneCount
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount -1) * laneWidth
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // $ vanilla magic selector
 
 import Car from './includes/Car'
+import Road from "./includes/Road";
 
 import {$} from './includes/Utils'
 
@@ -10,8 +11,8 @@ const canvas: HTMLCanvasElement = $('#canvas')
 canvas.width = 300
 
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
-
-const car: Car = new Car(100, 100, 40, 60)
+const road = new Road(canvas.width / 2, canvas.width * .94);
+const car: Car = new Car(road.getLaneCenter(1), 100, 40, 60)
 
 animate()
 
@@ -19,6 +20,7 @@ function animate() {
     car.update()
 
     canvas.height = window.innerHeight
+    road.draw(ctx)
     car.draw(ctx)
     requestAnimationFrame(animate)
 }
